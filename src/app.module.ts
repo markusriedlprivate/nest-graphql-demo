5ea57b92681abee80824c0c59771dd88bd255a81
Markus Riedl <markus.riedl.bewerber@gmail.com>
2023-05-07 11:15:54 +0200

    add moduleresolution

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { PetsModule } from './pets/pets.module';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -12,6 +13,12 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), '/src/schema.gql'),
       sortSchema: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: ':memory:',
+      entities: ['dist/**.*.entity{.ts,.js}'],
+      synchronize: true,
     }),
     PetsModule,
   ],
