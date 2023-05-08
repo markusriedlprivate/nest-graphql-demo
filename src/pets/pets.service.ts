0f7cad4ca6d5a06c3d14ffdeac6d57a75e3829cb
Markus Riedl <markus.riedl.bewerber@gmail.com>
2023-05-08 09:06:22 +0200

    add tutorial to readme

--- src/pets/pets.service.ts
+++ src/pets/pets.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { Pet } from './pet.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm/repository/Repository';
 
 @Injectable()
 export class PetsService {
+  constructor(@InjectRepository(Pet) private petsRepository: Repository<Pet>) {}
   async findAll(): Promise<Pet[]> {
-    const pet = new Pet();
-    pet.id = 1;
-    pet.name = 'Mambo';
-    return [pet];
+    // const pet = new Pet();
+    // pet.id = 1;
+    // pet.name = 'Mambo';
+    // return [pet];
+    return this.petsRepository.find(); // SELECT * FROM table
   }
 }
